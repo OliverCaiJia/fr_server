ae1351c757d8c26c4b63e57fa8e9284e378e81af
jackmaaidong <maaidong@sudaizhijia.com>
2018-11-02 18:35:25 +0800

    项目部署

---
+++ app/Models/Factory/Web/WebFactory.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models\Factory\Web;
+
+use App\Models\AbsModelFactory;
+
+class WebFactory extends AbsModelFactory
+{
+    /**
+     * 通过用户主键ID获取用户名
+     *
+     * @param $id
+     *
+     * @return mixed|string
+     */
+    public static function getNameById($id)
+    {
+
+    }
+}
