e2efca3ca52843d190a34d8c22bcf79da509e4b3
jackmaaidong <maaidong@sudaizhijia.com>
2018-11-09 18:28:20 +0800

    修改项目

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,7 +15,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        define("PRODUCTION_ENV", $this->app->environment('production'));
     }
 
     /**
