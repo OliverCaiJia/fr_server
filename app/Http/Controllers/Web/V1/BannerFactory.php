141759ef1054b375814209a1da3f4d8dbc6c7a42
jackmaaidong <maaidong@sudaizhijia.com>
2018-11-09 11:16:28 +0800

    短信，banner工厂

---
+++ app/Http/Controllers/Web/V1/BannerFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models\Factory;
+
+use App\Models\Factory\Api\ApiFactory;
+use App\Models\Orm\Banner;
+
+/**
+ * Class BanksFactory
+ * @package App\Models\Factory
+ * 银行卡工厂
+ */
+class BannerFactory extends ApiFactory
+{
+    /**
+     * @param $banner_type_id
+     * @return array
+     * 获取指定类型banner
+     */
+    public static function bannerList($banner_type_id)
+    {
+        $bannerList = Banner::select(['banner_name','position','img_address','img_href'])
+            ->where(['banner_type_id',$banner_type_id])
+            ->get()
+            ->toArray();
+
+        return $bannerList;
+    }
+
+
+}
