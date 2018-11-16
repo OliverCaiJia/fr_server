dc9a565da652b399795a5bb0eeb986b4677bb1f9
zhijie <caijia@sudaizhijia.com>
2018-11-16 21:12:25 +0800

    yibao controller

---
+++ app/Http/Controllers/Api/V1/YiBaoController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Helpers\RestResponseFactory;
+use App\Helpers\RestUtils;
+use App\Http\Controllers\Api\ApiController;
+use App\Models\Factory\Api\AccountFactory;
+use App\Models\Factory\Api\InviteFactory;
+use Illuminate\Http\Request;
+
+
+class YiBaoController extends ApiController
+{
+    /**
+     * 获取账户信息
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public  function sync(Request $request)
+    {
+        $uid = $this->getUserId($request);
+        $account = AccountFactory::fetchUserAccount($uid);
+        return RestResponseFactory::ok($account);
+    }
+
+    public function async(Request $request)
+    {
+        $uid = $this->getUserId($request);
+        $UserOrder = InviteFactory::fetchUserInvitations($uid);
+        return RestResponseFactory::ok($UserOrder);
+    }
+}
