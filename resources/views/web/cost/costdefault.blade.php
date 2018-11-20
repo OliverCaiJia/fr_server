4d68a4436bd51214ac2e6cfe89fe2ebd437f4396
chenmeng123 <chenmeng@sudaizhijia.com>
2018-11-20 15:44:10 +0800

    添加推荐服务页面

--- resources/views/web/cost/costdefault.blade.php
+++ resources/views/web/cost/costdefault.blade.php
@@ -1,1 +1,101 @@
-/web/cost/costdefault
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <link rel="icon" href="">
+    <link rel="stylesheet" href="{{ asset('css/reset.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/cost/costDefault.css') }}">
+    <title>水果贷-推荐服务</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="main">
+            <div class="banner"> <img src="/img/cost/banner.png" alt=""> </div>
+            <div class="recommend-list">
+                <h3 class="title">为您推荐：</h3>
+                <div class="service-list">
+                    <dl> <dt><img src="" alt=""></dt>
+                        <dd>
+                            <h4>借款20000元/3个月</h4>
+                            <p>放款快,周期长,利率低
+                                <br><span>￥<i>128</i></span>￥188</p>
+                        </dd> <span></span> </dl>
+                </div>
+            </div>
+            <div class="give-list">
+                <h3 class="title">您买我就送：</h3>
+                <div class="service-list">
+                    <dl> <dt><img src="" alt=""></dt>
+                        <dd>
+                            <h4>借款20000元/3个月</h4>
+                            <p>放款快,周期长,利率低
+                                <br><span>￥<i>128</i></span>￥188</p>
+                        </dd> <span class='selectIcon'></span> </dl>
+                </div>
+                <div class="service-list">
+                    <dl> <dt><img src="" alt=""></dt>
+                        <dd>
+                            <h4>借款20000元/3个月</h4>
+                            <p>放款快,周期长,利率低
+                                <br><span>￥<i>128</i></span>￥188</p>
+                        </dd> <span class='selectIcon'></span> </dl>
+                </div>
+            </div>
+        </div>
+        <footer>
+            <div class="left">
+                <h3>总计：<span>￥<i>188</i></span></h3>
+                <p>原价:￥378</p>
+            </div>
+            <div class="right"> 付款 </div>
+        </footer>
+    </div>
+    <script src="{{ asset('js/jquery-3.3.1.min.js') }}"></script>
+    <script>
+        var costdefatltController = {
+            init: function() {
+                this.selectView()
+            }, //选择赠送协议
+            selectView: function() {
+                var _self = this;
+                $('.service-list').on('click', function() {
+                    $(this).siblings('div').find('.selectIcon').hide();
+                    $(this).find('.selectIcon').show();
+                    _self.recommendService();
+                })
+            }, //选择赠送协议交互
+            recommendService: function() {
+                try {
+                    window.sd.recommendService();
+                    return;
+                } catch (e) {
+                    console.log("Android-选择赠送协议方法失败");
+                }
+                try {
+                    window.webkit.messageHandlers.recommendService.postMessage({});
+                    return;
+                } catch (e) {
+                    console.log("ios-选择赠送协议方法失败");
+                }
+                try {
+                    window.parent.postMessage({
+                        'type': 'recommendService'
+                    }, '*');
+                    return;
+                } catch (e) {
+                    console.log("h5-选择赠送协议方法返失败");
+                }
+            }
+        }
+        $(function() {
+            costdefatltController.init();
+        })
+
+    </script>
+</body>
+
+</html>
