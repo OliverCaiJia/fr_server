eee9c2336204b117c8bb38c64afaf21e4271bdb3
xxw <xingxuewen@sudaizhijia.com>
2018-11-20 18:50:22 +0800

    update

--- app/Http/Controllers/Api/V1/BannerController.php
+++ app/Http/Controllers/Api/V1/BannerController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Helpers\Http\HttpClient;
 use App\Helpers\RestResponseFactory;
+use App\Helpers\RestUtils;
 use App\Http\Controllers\Api\ApiController;
 use App\Services\Core\Validator\ValidatorService;
 use App\Models\Factory\Api\BannerFactory;
