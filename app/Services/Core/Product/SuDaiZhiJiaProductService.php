7c91d77cb3d9d56459cacf1d76484c88564ff088
jackmaaidong <maaidong@sudaizhijia.com>
2018-11-20 14:12:20 +0800

    速贷之家产品接口service

---
+++ app/Services/Core/Product/SuDaiZhiJiaProductService.php
@@ -0,0 +1,43 @@
+<?php
+namespace App\Services\Core\Product;
+
+use App\Helpers\Http\HttpClient;
+use App\Services\AppService;
+
+/**
+ * Class PushService
+ * @package App\Services\Core\Store\Push
+ * 推送
+ */
+class SuDaiZhiJiaProductService extends AppService
+{
+    /**
+     * 获取速贷之家合作贷产品数据
+     * @param array $params
+     * @return Json
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public static function productCooperate($data = [])
+    {
+        //获取config
+        $url = SuDaiZhiJiaProductConfig::SUDAIZHIJIA_PRODUCT_URL."?pageSize={$data['pageSize']}&pageNum={$data['pageNum']}&productType=1&terminalType={$data['terminalType']}";
+        $response = HttpClient::i()->request('GET', $url);
+        $result = $response->getBody()->getContents();
+        return $result;
+    }
+
+    /**
+     * 速贷之家立即申请产品url
+     * @param array $data
+     * @return Json
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public static function cooperateApplication($data = [])
+    {
+        //获取config
+        $url = SuDaiZhiJiaProductConfig::SUDAIZHIJIA_APPLY_URL."?productId={$data['productId']}&typeId={$data['typeId']}";
+        $response = HttpClient::i()->request('GET', $url);
+        $result = $response->getBody()->getContents();
+        return $result;
+    }
+}
