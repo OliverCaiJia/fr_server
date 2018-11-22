348c4cf360aa087ffea3e94a064269c7c31df8a4
chenlong <chenlong@sudaizhijia.com>
2018-11-22 23:24:31 +0800

    支付测试

--- app/Services/Core/Payment/YiBao/YiBaoConfig.php
+++ app/Services/Core/Payment/YiBao/YiBaoConfig.php
@@ -23,10 +23,11 @@ class YiBaoConfig
 
     //服务器服务器回调地址
 //    const NOTIFYURL = 'http://uat.fruit.witlending.com/api/v1/callback/yibao/async';
-    const NOTIFYURL = 'http://47.104.253.50/api/v1/callback/yibao/async';
+    const NOTIFYURL = 'http://data.witlending.com/callback/yibao/async';
 
     //服务器页面回调地址
-    const REDIRECTURL = 'http://uat.fruit.witlending.com/api/v1/callback/yibao/sync';
+//    const REDIRECTURL = 'http://uat.fruit.witlending.com/api/v1/callback/yibao/sync';
+    const REDIRECTURL = 'http://data.witlending.com/callback/yibao/sync';
 
     //易宝url
     const SERVERROOT = "https://open.yeepay.com/yop-center";
