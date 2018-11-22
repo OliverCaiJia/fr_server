f7ed20914cc2dc401122098d1c826c1f9331d7e1
JuKun <jukunmail@163.com>
2018-11-22 18:32:17 +0800

    OSS config file.

---
+++ config/oss.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    /**
+     *  阿里OSS
+     */
+    'ali-oss' => [
+        'address' => 'oss-cn-zhangjiakou.aliyuncs.com',
+        'bucket' => 'fruitloan',
+        'AccessKeyId' => 'LTAIsYqdB9L4l85J',
+        'AccessKeySecret' => 'y6tkU59sczghd9N8Qh4OaAD79HmYD7',
+    ]
+];
