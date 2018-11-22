075e35d4f15835001ae2f9ce9bc8759b160ff0df
chenlong <chenlong@sudaizhijia.com>
2018-11-22 17:25:00 +0800

    支付测试

--- app/Http/Controllers/Api/V1/TestController.php
+++ app/Http/Controllers/Api/V1/TestController.php
@@ -42,7 +42,7 @@ class TestController extends ApiController
 //        $data = YiJianDaiPushService::o()->getPull($data);
 //        print_r($data);die;
         $data = [
-            'orderId' => 'DS181122_17224974',
+            'orderId' => 'DS181122_17243527',
             'orderAmount' => '0.01',
             'goodsParamExt' => '{"goodsName":"水果贷测试","goodsDesc":"水果贷订单"}',
             'paymentParamExt' => '{"bankCardNo":"6212260200101725345","idCardNo":"610303197911112419","cardName":"巨琨"}',
