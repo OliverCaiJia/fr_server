04e40245ef53d53d772d6d781faa4745e8a3b1eb
chenlong <chenlong@sudaizhijia.com>
2018-11-23 14:57:27 +0800

    支付测试

--- app/Http/Controllers/Api/V1/YiBaoController.php
+++ app/Http/Controllers/Api/V1/YiBaoController.php
@@ -5,9 +5,9 @@ namespace App\Http\Controllers\Api\V1;
 use App\Helpers\RestResponseFactory;
 use App\Helpers\RestUtils;
 use App\Http\Controllers\Api\ApiController;
-use App\Models\Factory\Api\AccountFactory;
-use App\Models\Factory\Api\InviteFactory;
 use Illuminate\Http\Request;
+use App\Services\Core\Payment\YiBao\YiBaoConfig;
+use App\Services\Core\Payment\YiBao\YopSignUtils;
 
 
 class YiBaoController extends ApiController
@@ -19,19 +19,16 @@ class YiBaoController extends ApiController
      */
     public  function sync(Request $request)
     {
-        $params = $request->all();
-        file_put_contents('1.txt',$params);die;
-//        $uid = $this->getUserId($request);
-//        $account = AccountFactory::fetchUserAccount($uid);
-//        return RestResponseFactory::ok($account);
+        $params = $request->get();
+        file_put_contents('1.txt',$params);
     }
 
     public function async(Request $request)
     {
         $params = $request->all();
-        file_put_contents('2.txt',$params);die;
-        $uid = $this->getUserId($request);
-        $UserOrder = InviteFactory::fetchUserInvitations($uid);
-        return RestResponseFactory::ok($UserOrder);
+        $public_key = YiBaoConfig::YOP_PUBLIC_KEY;
+        $private_key = YiBaoConfig::PRIVATE_KEY;
+        $data = YopSignUtils::decrypt($params,$public_key,$private_key);
+        file_put_contents('2.txt', $data);
     }
 }
