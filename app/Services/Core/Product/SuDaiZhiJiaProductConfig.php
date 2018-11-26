b5e0714569a1d7d06214828d7d2e1b17ac5df32a
jackmaaidong <maaidong@sudaizhijia.com>
2018-11-26 11:06:30 +0800

    速贷之家server修改

--- app/Services/Core/Product/SuDaiZhiJiaProductConfig.php
+++ app/Services/Core/Product/SuDaiZhiJiaProductConfig.php
@@ -13,7 +13,7 @@ namespace App\Services\Core\Product;
 class SuDaiZhiJiaProductConfig
 {
 
-    const SUDAIZHIJIA_PRODUCT_URL = 'https://uat.api.sudaizhijia.com/v3/products'; //速贷之家合作贷产品url(测试)
+    const SUDAIZHIJIA_PRODUCT_URL = 'https://uat.api.sudaizhijia.com/v5/products'; //速贷之家合作贷产品url(测试)
 
     const SUDAIZHIJIA_APPLY_URL = 'https://uat.api.sudaizhijia.com/v1/product/web/url';//速贷之家立即申请产品url(测试)
 
