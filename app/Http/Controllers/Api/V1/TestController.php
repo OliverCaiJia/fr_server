02061a486b6b00f50caa18ba215ace1ded79e9c4
chenlong <chenlong@sudaizhijia.com>
2018-11-27 09:57:23 +0800

    测试

--- app/Http/Controllers/Api/V1/TestController.php
+++ app/Http/Controllers/Api/V1/TestController.php
@@ -15,40 +15,11 @@ class TestController extends ApiController
 {
     //易宝支付
     public function test(){
-//        $data = MozhangService::o()->getMoZhangContent('陈龙','142201199606297437','15210029967','application');
-//        dd($data);die;
-//        $data = [
-//            'mobile' => '15210029967',
-//            'name' => '陈龙',
-//            'certificate_no' => '142201197007047437',
-//            'sex' => '1',
-//            'birthday' => '19950405',
-//            'has_insurance' => '1',
-//            'house_info' => '001',
-//            'car_info' => '001',
-//            'occupation' => '001',
-//            'salary_extend' => '001',
-//            'salary' => '105',
-//            'accumulation_fund' => '001',
-//            'work_hours' => '001',
-//            'business_licence' => '001',
-//            'has_creditcard' => '1',
-//            'social_security' => '1',
-//            'is_micro' => '1',
-//            'city' => '北京市',
-//            'money' => '100000',
-//        ];
-//        $data = [
-//            'mobile' => '15210029967'
-//        ];
-//        $data = YiJianDaiPushService::o()->getPull($data);
-//        print_r($data);die;
         $data = [
             'orderId' => 'DS181126_11511192',
             'orderAmount' => '0.01',
             'goodsParamExt' => '{"goodsName":"水果贷测试","goodsDesc":"水果贷订单"}',
-//            'paymentParamExt' => '{"bankCardNo":"6212260200101725345","idCardNo":"610303197911112419","cardName":"巨琨"}',
-            'paymentParamExt' => '{"bankCardNo":"6216910108587723","idCardNo":"142201199606297437","cardName":"陈龙"}',
+            'paymentParamExt' => '{"bankCardNo":"6212260200101725345","idCardNo":"610303197911112419","cardName":"巨琨"}',
             'userNo' => '15210029967',
         ];
         $url = YiBaoService::send($data);
