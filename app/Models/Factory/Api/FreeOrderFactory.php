02bdb42b6ddf72d78629ddfc978dba123a81aa51
xxw <xingxuewen@sudaizhijia.com>
2018-11-27 14:18:33 +0800

    去借钱生成免费订单

--- app/Models/Factory/Api/FreeOrderFactory.php
+++ app/Models/Factory/Api/FreeOrderFactory.php
@@ -15,22 +15,27 @@ class FreeOrderFactory extends ApiFactory
 {
     public static function Order($uid, $amount, $term)
     {
-        $userOrder = new UserOrder();
-        $userOrder->user_id = $uid;
-        $userOrder->order_no = UserOrderStrategy::createOrderNo();
-        $userOrder->order_type = 2;
-        $userOrder->p_order_id = 0;
-        $userOrder->order_expired = date('Y-m-d H:i:s', strtotime('+1 hour'));;
-        $userOrder->amount = $amount;
-        $userOrder->term = $term;
-        $userOrder->count = 1;
-        $userOrder->status = 0;
-        $userOrder->create_ip = Utils::ipAddress();;
-        $userOrder->create_at = date('Y-m-d H:i:s', time());
-        $userOrder->update_ip = Utils::ipAddress();;
-        $userOrder->update_at = date('Y-m-d H:i:s', time());
-        $data = $userOrder->save();
-        return $data ? $data : 0;
+        $orderStatus = UserOrder::where(['user_id' => $uid])->whereIn('status',array(0,1))->get()->toArray();
+        if (empty($orderStatus)) {
+            $userOrder = new UserOrder();
+            $userOrder->user_id = $uid;
+            $userOrder->order_no = UserOrderStrategy::createOrderNo();
+            $userOrder->order_type = 2;
+            $userOrder->p_order_id = 0;
+            $userOrder->order_expired = date('Y-m-d H:i:s', strtotime('+1 hour'));;
+            $userOrder->amount = $amount;
+            $userOrder->term = $term;
+            $userOrder->count = 1;
+            $userOrder->status = 0;
+            $userOrder->create_ip = Utils::ipAddress();;
+            $userOrder->create_at = date('Y-m-d H:i:s', time());
+            $userOrder->update_ip = Utils::ipAddress();;
+            $userOrder->update_at = date('Y-m-d H:i:s', time());
+            $data = $userOrder->save();
+            return $data ? $data : 0;
+        } else {
+            return $orderStatus ? $orderStatus : [];
+        }
     }
 
 }
