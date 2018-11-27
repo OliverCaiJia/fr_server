a838d369d878f25f9f8f1aeb8d04338ea3f8dd8d
jackmaaidong <maaidong@sudaizhijia.com>
2018-11-27 19:45:16 +0800

    订单过期状态跑批

--- config/order.php
+++ config/order.php
@@ -3,9 +3,8 @@
 return [
     /**
      * 订单类型配置
-     * 付费订单：pay_order 免费订单：free_order
+     * 付费订单：pay_order
      */
 
     'pay_order' => ['order_report','order_extra_service'],
-    'free_order' => ['order_apply','order_product'],
 ];
