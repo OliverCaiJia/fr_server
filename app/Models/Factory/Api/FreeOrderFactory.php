714591960f80c95cfb1077097b655b41f97a42de
xxw <xingxuewen@sudaizhijia.com>
2018-11-30 16:41:51 +0800

    修改接口

--- app/Models/Factory/Api/FreeOrderFactory.php
+++ app/Models/Factory/Api/FreeOrderFactory.php
@@ -14,39 +14,16 @@ use App\Helpers\Utils;
  */
 class FreeOrderFactory extends ApiFactory
 {
-    public static function Order($uid, $amount, $term)
+    public static function Order($uid)
     {
-        $orderStatus = UserLoanTask::where(['user_id' => $uid])->first();
-        //修改的时间
-        $register = strtotime($orderStatus->update_at);
-        //30天过期时间
-        $endtime = 30 * 24 * 60 * 60;
-        //当前时间
-        $nowtime = time();
-        if (($nowtime - $register) > $endtime) {
-            $userOrder = new UserOrder();
-            $userOrder->user_id = $uid;
-            $userOrder->order_no = UserOrderStrategy::createOrderNo();
-            $userOrder->order_type = 2;
-            $userOrder->p_order_id = 0;
-            $userOrder->order_expired = date('Y-m-d H:i:s', strtotime('+1 hour'));
-            $userOrder->amount = $amount;
-            $userOrder->term = $term;
-            $userOrder->count = 1;
-            $userOrder->status = 1;
-            $userOrder->create_ip = Utils::ipAddress();
-            $userOrder->create_at = date('Y-m-d H:i:s', time());
-            $userOrder->update_ip = Utils::ipAddress();
-            $userOrder->update_at = date('Y-m-d H:i:s', time());
-            $data = $userOrder->save();
-            if ($data == 1) {
-                $FreeOrder = UserOrder::where(['user_id' => $uid])->first();
-                return $FreeOrder ? $FreeOrder->toArray() : [];
+        $orderStatus = UserLoanTask::where(['user_id' => $uid, 'status' => 0])->first();
+        //如果等于
+        if (!empty($orderStatus)) {
+            $orderStatus->status = 1;
+            $orderStatus->update_at = date('Y-m-d H:i:s');
+            if ($orderStatus->save()) {
+                return $orderStatus->toArray();
             }
-        } else {
-            $FreeOrder = UserOrder::where(['user_id' => $uid])->first();
-            return $FreeOrder ? $FreeOrder->toArray() : [];
         }
     }
-
 }
