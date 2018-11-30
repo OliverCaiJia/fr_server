1ceb100896059d157b20eda9ab01b4c90e6195bb
zhijie <caijia@sudaizhijia.com>
2018-11-30 20:19:49 +0800

    log

--- app/Models/Chain/Report/Scopion/CreateReportLogAction.php
+++ app/Models/Chain/Report/Scopion/CreateReportLogAction.php
@@ -90,8 +90,10 @@ class CreateReportLogAction extends AbstractHandler
             $reportLog['data'] = json_encode($params['credit_qualification']);
 
             $reportLog = UserOrderFactory::createReportLog($reportLog);
+            SLogger::getStream()->error(__CLASS__.'======34343434343'.json_encode($params));
+
             $userAmountEst = ReportStrategy::getAmountEst($params);
-            SLogger::getStream()->error(__CLASS__.'======'.json_encode($userAmountEst));
+            SLogger::getStream()->error(__CLASS__.'======2121212121212121'.json_encode($userAmountEst));
             UserOrderFactory::createAmountEst($userAmountEst);
             SLogger::getStream()->error(__CLASS__.'===--------------===');
         }
