2a4353b2c9b779f07cff51c91b4e3b71c6456517
JuKun <jukunmail@163.com>
2018-11-30 18:22:00 +0800

    Update

--- app/Services/Core/Message/Sms/Chuanglan/ChuanglanConfig.php
+++ app/Services/Core/Message/Sms/Chuanglan/ChuanglanConfig.php
@@ -3,8 +3,8 @@ namespace App\Services\Core\Message\Sms\Chuanglan;
 
 class ChuanglanConfig
 {
-    const CHUANGLAN_API_URL = 'https://smssh1.253.com/msg/send/json';//创蓝api接口地址
-    const CHUANGLAN_API_ACCOUNT = 'N6653401';//创蓝api账号
-    const CHUANGLAN_API_PASSWORD = 'OpldWYPvLaaf6a';//创蓝api密码
+    const CHUANGLAN_API_URL = PRODUCTION_ENV ? 'https://smssh1.253.com/msg/send/json': 'https://smssh1.253.com/msg/send/json';//创蓝api接口地址
+    const CHUANGLAN_API_ACCOUNT = PRODUCTION_ENV ? 'N6653401': 'N6653401';//创蓝api账号
+    const CHUANGLAN_API_PASSWORD = PRODUCTION_ENV ? 'OpldWYPvLaaf6a': 'OpldWYPvLaaf6a';//创蓝api密码
 
 }
