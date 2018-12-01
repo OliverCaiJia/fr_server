7268405f494bbbbb4c19e2569196249b7174fa2e
jackmaaidong <maaidong@sudaizhijia.com>
2018-12-01 19:51:55 +0800

    订单过期状态跑批

--- app/Console/Commands/OrderStatusCommand.php
+++ app/Console/Commands/OrderStatusCommand.php
@@ -45,28 +45,23 @@ class OrderStatusCommand extends Command
      */
     public function handle()
     {
-        $pay_order = config("order.pay_order"); //付费订单
-        $orderTypeList = UserOrder::join('user_order_type','user_order.order_type','=','user_order_type.id')
+        UserOrder::join('user_order_type','user_order.order_type','=','user_order_type.id')
             ->select('user_order.status','user_order.create_at','user_order_type.type_nid','user_order.user_id','user_order.order_no')
             ->where('user_order.status','=',0)
             ->where('user_order_type.status','=',1)
-            ->get()
-            ->toArray();
-        if(empty($orderTypeList)){
-            return;
-        }
-
-        $data = [
-            'status' => 2,
-            'update_ip' => Utils::ipAddress(),
-        ];
-
-        foreach($orderTypeList as $order_key => $order_val){
-            $pay_create_at = strtotime("{$order_val['create_at']}+7 day");
-            if($pay_create_at <= strtotime(date('Y-m-d H:i:s')) && in_array($order_val['type_nid'],$pay_order)){
-                $data['update_at'] = date('Y-m-d H:i:s');
-                UserOrderFactory::updateOrderByUserIdAndOrderNo($order_val['user_id'],$order_val['order_no'],$data);
-            }
-        }
+            ->chunk(100,function ($orderTypeList){
+                $data = [
+                    'status' => 2,
+                    'update_ip' => Utils::ipAddress(),
+                ];
+                $pay_order = config("order.pay_order"); //付费订单
+                foreach($orderTypeList as $order_key => $order_val){
+                    $pay_create_at = strtotime("{$order_val['create_at']}+7 day");
+                    if($pay_create_at <= strtotime(date('Y-m-d H:i:s')) && in_array($order_val['type_nid'],$pay_order)){
+                        $data['update_at'] = date('Y-m-d H:i:s');
+                        UserOrderFactory::updateOrderByUserIdAndOrderNo($order_val['user_id'],$order_val['order_no'],$data);
+                    }
+                }
+            });
     }
 }
