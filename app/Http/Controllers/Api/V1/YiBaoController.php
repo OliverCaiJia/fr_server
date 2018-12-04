2f0736287e87107eed3dfc00f37bb6be7f43dcf2
zhijie <caijia@sudaizhijia.com>
2018-12-04 15:20:39 +0800

    strategy

--- app/Http/Controllers/Api/V1/YiBaoController.php
+++ app/Http/Controllers/Api/V1/YiBaoController.php
@@ -3,15 +3,10 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Helpers\Logger\SLogger;
-use App\Helpers\RestResponseFactory;
-use App\Helpers\RestUtils;
 use App\Http\Controllers\Api\ApiController;
 use App\Models\Chain\Order\NoPayOrder\LoanOrder\DoApplyOrderHandler;
 use App\Models\Chain\Order\PayOrder\PaidOrder\DoPaidOrderHandler;
 use App\Models\Chain\Payment\PaymentAccount\DoPaymentAccountHandler;
-use App\Models\Factory\Api\UserinfoFactory;
-use App\Models\Factory\Api\UserOrderFactory;
-use App\Strategies\UserOrderStrategy;
 use Illuminate\Http\Request;
 use App\Services\Core\Payment\YiBao\YiBaoConfig;
 use App\Services\Core\Payment\YiBao\YopSignUtils;
@@ -31,7 +26,7 @@ class YiBaoController extends ApiController
      * @param Request $request
      * @return \Illuminate\Http\JsonResponse
      */
-    public function sync(Request $request)
+    public  function sync(Request $request)
     {
         return 'ERROR';
     }
@@ -48,22 +43,58 @@ class YiBaoController extends ApiController
         //获取配置信息
         $public_key = YiBaoConfig::YOP_PUBLIC_KEY;
         $private_key = YiBaoConfig::PRIVATE_KEY;
-        $resData = YopSignUtils::decrypt($params, $private_key, $public_key);
-        $resData = json_decode($resData, true);
+        $resData = YopSignUtils::decrypt($params,$private_key,$public_key);
+        $resData = json_decode($resData,true);
         //订单支付成功
-        if ($resData['status'] != 'SUCCESS') {
+        if($resData['status'] != 'SUCCESS'){
             return 'ERROR';
         }
         //获取订单编号
         $data['order_no'] = $resData['orderId'];
-        $userOrder = UserOrderFactory::getUserOrderByOrderNo($data['order_no']);
-        if (empty($userOrder)) {
-            return RestResponseFactory::ok(RestUtils::getStdObj(), '未找到该订单', 12345, '未找到该订单');
+        //事务处理
+        DB::beginTransaction();
+        try
+        {
+            //修改订单状态
+            $orderTypeChain = new DoPaidOrderHandler($data);
+            $typeRes = $orderTypeChain->handleRequest();
+            if(isset($typeRes['error'])){
+                return 'ERROR';
+            }
+
+            //生成信用报告
+            $data['report_type_nid'] = $typeRes['report_type_nid'];
+            $reportChain = new DoReportOrderHandler($data);
+            $reportRes = $reportChain->handleRequest();
+            if(isset($reportRes['error'])){
+                return 'ERROR';
+            }
+
+            //推送一键贷
+            $task = new DoApplyOrderHandler($data);
+            $taskRes = $task->handleRequest();
+            if(isset($taskRes['error'])){
+                return 'ERROR';
+            }
         }
-        $orderType = UserOrderFactory::getOrderTypeNidByTypeId($userOrder['order_type']);
-        UserOrderStrategy::getChainsByTypeNid($orderType['type_nid'], $data, $resData, $userOrder['user_id']);
+        catch (\Exception $e)
+        {
+            //订单异常事务回滚
+            DB::rollBack();
+            Log::error($e);
+            //异常日志记录
+            SLogger::getStream()->error('支付回调, 支付回调订单-catch');
+            SLogger::getStream()->error($e->getMessage());
+            return 'ERROR';
+        }
+        //事务提交
+        DB::commit();
 
+        //记录
+        $paymentChain = new DoPaymentAccountHandler($resData);
+        $paymentChain->handleRequest();
 
+        return 'SUCCESS';
 
     }
 }
