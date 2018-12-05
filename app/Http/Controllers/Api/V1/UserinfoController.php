70a961dbfeea77f7431ed0ab784adafe6650c928
chenlong <chenlong@sudaizhijia.com>
2018-12-05 16:32:11 +0800

    接口测试

--- app/Http/Controllers/Api/V1/UserinfoController.php
+++ app/Http/Controllers/Api/V1/UserinfoController.php
@@ -48,6 +48,7 @@ class UserInfoController extends ApiController
 //            'create_at' => '2018-11-12 15:41:16',
 //            'update_at' => '2018-11-12 15:41:16',
 //        ];
+        SLogger::getStream()->info('data', ['data' =>$data]);
         $UserBasic = UserBasicFactory::createOrUpdateUserBasic($data, $uid);
         if ($UserBasic) {
             return RestResponseFactory::ok($data);
