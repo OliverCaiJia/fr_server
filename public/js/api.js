efbfc97f33fd576e228354f7f60c5b2c54024989
chenmeng123 <chenmeng@sudaizhijia.com>
2018-12-08 10:48:21 +0800

    修改api地址

--- public/js/api.js
+++ public/js/api.js
@@ -3,9 +3,10 @@ var host = window.location.host;
 var arr = host.split(".");
 var host_href = arr[arr.length - 2];
 var api_fruitloan_host;
-if (host == "api." + host_href + ".com") {
+if (host == "app.haoshijinfu.com") {
     var fruit_protocol = (("https:" == document.location.protocol) ? "https://" : "http://");
-} else {
+}
+else {
     var fruit_protocol = (("https:" == document.location.protocol) ? "https://uat." : "http://uat.");
     //    document.write('<script src="/js/vconsole.min.js"></script>');
 }
@@ -19,11 +20,13 @@ function utf16to8(str) {
         c = str.charCodeAt(i);
         if ((c >= 0x0001) && (c <= 0x007F)) {
             out += str.charAt(i);
-        } else if (c > 0x07FF) {
+        }
+        else if (c > 0x07FF) {
             out += String.fromCharCode(0xE0 | ((c >> 12) & 0x0F));
             out += String.fromCharCode(0x80 | ((c >> 6) & 0x3F));
             out += String.fromCharCode(0x80 | ((c >> 0) & 0x3F));
-        } else {
+        }
+        else {
             out += String.fromCharCode(0xC0 | ((c >> 6) & 0x1F));
             out += String.fromCharCode(0x80 | ((c >> 0) & 0x3F));
         }
@@ -32,9 +35,9 @@ function utf16to8(str) {
 }
 $.ajaxSetup({
     beforeSend: function (xhr) {
-        var $token = $('#sign').html() || $('.token').html(),
-            url = $.trim(this.url),
-            type = this.type.toUpperCase();
+        var $token = $('#sign').html() || $('.token').html()
+            , url = $.trim(this.url)
+            , type = this.type.toUpperCase();
         for (var i = -1, arr = [];
             (i = url.indexOf("?", i + 1)) > -1;) {
             arr.push(i);
@@ -44,15 +47,17 @@ $.ajaxSetup({
             if (arr.length != 0) {
                 var dataString = url.substring(arr).replace('?', '');
                 var url = url.substring(0, arr);
-            } else {
+            }
+            else {
                 var dataString = "";
             }
-        } else {
+        }
+        else {
             var dataString = decodeURI(this.data);
         }
-        var $signUrl = hex_sha1(url),
-            dataArr = dataString.split('&'),
-            $dataArr = [];
+        var $signUrl = hex_sha1(url)
+            , dataArr = dataString.split('&')
+            , $dataArr = [];
         for (var i = 0; i < dataArr.length; i++) {
             var fir = dataArr[i].indexOf('=');
             var arr = dataArr[i].split('');
@@ -63,8 +68,8 @@ $.ajaxSetup({
         $dataString = $dataArr.sort().join(''), $startString = $dataString.substring(0, 3), $endString = $dataString.substring($dataString.length - 3), $sha1Text = $startString + $token + $endString + $signUrl, $sha1Sign = hex_sha1(utf16to8($sha1Text));
         xhr.setRequestHeader("X-Sign", $sha1Sign);
         xhr.setRequestHeader("X-Token", $token);
-    },
-    error: function (jqXHR, textStatus, errorMsg) {
+    }
+    , error: function (jqXHR, textStatus, errorMsg) {
         base.popupCover({
             content: '服务器繁忙，请稍后重试！'
         })
