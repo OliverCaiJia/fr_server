d4348007bba8f77f2108107c45361b41d2533c9a
chenmeng123 <chenmeng@sudaizhijia.com>
2018-12-08 17:58:07 +0800

    样式优化

--- resources/views/web/user/createreport.blade.php
+++ resources/views/web/user/createreport.blade.php
@@ -13,6 +13,9 @@
         body {
             width: 100%;
             height: 100%;
+            overflow: hidden;
+            overflow-y: scroll;
+            -webkit-overflow-scrolling: touch;
         }
 
         .container {
