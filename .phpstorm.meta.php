4c7c6380ab2906379e6579559ffd42f215542d5f
hanqiang <lianghanqiang@sudaizhijia.com>
2018-12-13 16:19:21 +0800

    {T576} update gitignore
    Summary: {T576} update gitignore
    
    Test Plan: none
    
    Reviewers: caijia, maaidong, chenlong
    
    Reviewed By: caijia
    
    Subscribers: chenlong, maaidong, caijia
    
    Differential Revision: http://review.sudaizhijia.com/D987

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2018-11-22 16:46:46.
+    * Generated on 2018-12-13 16:17:50.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
